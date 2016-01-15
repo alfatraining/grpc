749e01b9c90609a6c713e8e8dac8731acc3b4b29
Konstantinos Sofokleous <ksophocleous@gmail.com>
2016-01-15 12:23:37 +0100

    add option to disable openssl init add more tests to cmake
    the disabling of openssl init is useful when you are using grpc with another library (e.g. Qt)
    that is also using openssl (statically linked). There's the problem with both initializing openssl
    (and both using their own mutex locks which changing them during a lock, most likely spells disaster)

--- src/core/tsi/ssl_transport_security.c
+++ src/core/tsi/ssl_transport_security.c
@@ -130,6 +130,7 @@ static unsigned long openssl_thread_id_cb(void) {
 }
 
 static void init_openssl(void) {
+#ifndef DISABLE_OPENSSL_INIT
   int i;
   int num_locks;
   SSL_library_init();
@@ -144,6 +145,9 @@ static void init_openssl(void) {
   }
   CRYPTO_set_locking_callback(openssl_locking_cb);
   CRYPTO_set_id_callback(openssl_thread_id_cb);
+#else
+  gpr_log(GPR_INFO, "openssl init is disabled by build system");
+#endif
 }
 
 /* --- Ssl utils. ---*/
